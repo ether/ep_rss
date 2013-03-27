a27028c5c7c5c03f02e401280a2b58790e80cd1e
John McLear <john@mclear.co.uk>
2013-03-27 15:14:16 +0000

    somethign semi working, but doesn't update in a timely fashion

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var db = require('ep_etherpad-lite/node/db/DB').db;
-var async = require('ep_etherpad-lite/node_modules/async');
+    var db = require('ep_etherpad-lite/node/db/DB').db,
+       ERR = require("ep_etherpad-lite/node_modules/async-stacktrace"),
+       API = require('../../src/node/db/API.js'),
+     async = require('ep_etherpad-lite/node_modules/async'),
+padManager = require('ep_etherpad-lite/node/db/PadManager');
 
 exports.eejsBlock_htmlHead = function (hook_name, args, cb){
   args.content = args.content + '<link rel="alternate" type="application/rss+xml" title="Pad RSS Feed" href="feed" />';
@@ -9,38 +12,69 @@ exports.eejsBlock_htmlHead = function (hook_name, args, cb){
 exports.registerRoute = function (hook_name, args, cb) {
 
   console.warn("Registering route")
-  args.app.get('/p/*/feed', function(req, res) {
+
+  args.app.get('/p/*/feed.rss', function(req, res){
+    /*Sanity is in the crack of time*/
     var path=req.url.split("/");
     var padId=path[2];
+    var padURL = req.protocol + "://" + req.get('host') + "/p/" +padId;
+    res.redirect('/p/'+padId+'/feed');
+  });
 
-    console.warn(padId);
 
-    // has pad been edited in last hour?
+  args.app.get('/p/*/feed', function(req, res) {
+    /*Sanity is in the cracks of lime*/
+    var fullURL = req.protocol + "://" + req.get('host') + req.url;
+    var path=req.url.split("/");
+    var padId=path[2];
+    var padURL = req.protocol + "://" + req.get('host') + "/p/" +padId;
+    var dateString = new Date();
 
-/*
-    var searchString = req.query["query"];
-    var result = {};
-
-    db.findKeys("pad:*", "*:*:*", function(err, pads){ // get all pads
-
-      async.forEachSeries(pads, function(pad, callback){
-
-        db.get(pad, function(err, padData){ // get the pad contents
-          var padText = padData.atext.text || "";
-          // does searchString exist in aText?
-          if (padText.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
-            result.pad = pad;
-          }
+    var pad;
+    async.series([
+      function(callback){ // Get the pad Text
+        var padText = padManager.getPad(padId, function(err, _pad){
+          pad = _pad;
+          text = safe_tags(pad.text()).replace(/\n/g,"<br/>");
+          ERR(err);
           callback();
         });
-       
-      }, function(err){
-        res.send(JSON.stringify(result));
-      });
+      },
 
-    });
-*/
-    res.send("RSS feed coming soon");
+      function(callback){ // Append the pad Text to the Body
+
+        /* Why don't we use EEJS require here?  Well EEJS require isn't ASYNC so on first load
+        it would bring in the .ejs content only and then on second load pad contents would be included..
+        Sorry this is ugly but that's how the plugin FW was designed by @redhog -- bug him for a fix! */
+
+        res.contentType("rss");
+        args.content = '<rss xmlns:media="'+fullURL+'" version="2.0">\n';
+        args.content += '<channel>\n';
+        args.content += '<title>'+padId+'</title>\n';
+        args.content += '<description/>\n';
+        args.content += '<language>en-us</language>\n';
+        args.content += '<pubDate>'+dateString+'</pubDate>\n';
+        args.content += '<lastBuildDate>'+dateString+'</lastBuildDate>\n';
+        args.content += '<item>\n';
+        args.content += '<title>\n';
+        args.content += padId + ' has been changed\n';
+        args.content += '</title>\n';
+        args.content += '<description>\n';
+        args.content += '<![CDATA['+text+']]>\n';
+        args.content += '<link>'+padURL+'</link>\n';
+        args.content += '</item>\n';
+        args.content += '</channel>\n';
+        args.content += '</rss>';
+        res.send(args.content);
+        callback(); // Am I even called?
+      },
+    ]);
+
+    // res.send("RSS feed coming soon");
 
   });
 };
+
+function safe_tags(str) {
+  return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') ;
+}
